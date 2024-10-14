ad788f410be294c2001710647990b2aa3efda180
mohammedhejazi98 <64383814+mohammedhejazi98@users.noreply.github.com>
2024-10-15 00:21:44 +0300

    Add BusinessCard interface
    Create a new BusinessCard interface to define the structure of business card objects. This includes properties for id, name, email, gender, phone, photo as Base64 string, date of birth, and address.

---
+++ src/app/models/BusinessCard .ts
@@ -0,0 +1,10 @@
+export interface BusinessCard {
+  id: number;
+  name: string;
+  email: string;
+  gender: string;
+  phone: string;
+  photoBase64: string | null;
+  dateOfBirth: string;
+  address: string;
+}
